ccf4e6c539d11a406e42e7e384044ace61d21eb3
Zlatlan <mackenly1999@gmail.com>
2024-11-13 15:14:11 +0100

    upload files

---
+++ JADEN/JAD.py
@@ -0,0 +1,23 @@
+#%% Importeer de benodigde bibliotheken
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import streamlit as st
+import os
+import requests
+import folium
+from PIL import Image
+from streamlit_folium import st_folium 
+from folium.plugins import MarkerCluster
+from folium.plugins import HeatMap
+from sklearn.ensemble import RandomForestRegressor
+
+#%% test
+
+def greet():
+    return "Hello from Jaden!"
+
+#%%
+
